134b8c76e483a0bb51ce397f5bd8f42fa67a641d
starthks <starthks@gmail.com>
2024-01-14 15:06:48 +0900

    insert

--- earlygo/example/main.go
+++ earlygo/example/main.go
@@ -4,20 +4,20 @@ import eg "earlygo"
 
 func main() {
 	/*
-	eg.S10() //channel
-	eg.S11() //sum
-	eg.S12() //Mutex 연산시간 보장시 일정한 값
-	eg.S13() //rwMutex
-	eg.S14() //cond.Signal() 
-	eg.S15() //once.Do
-	eg.S16() // sync.Pool
-	eg.S17() // sync.WaitGroup
-	eg.S18() // atomic.AddInt32(&data, -1)
-	eg.S19() // reflect.TypeOf, reflect.ValueOf
-	eg.S20() // 동적함수
-	eg.S21() // Scanf
-	eg.S22() // Fscanf
-	eg.S23() // utf8.RuneLen
+		eg.S10() //channel
+		eg.S11() //sum
+		eg.S12() //Mutex 연산시간 보장시 일정한 값
+		eg.S13() //rwMutex
+		eg.S14() //cond.Signal()
+		eg.S15() //once.Do
+		eg.S16() // sync.Pool
+		eg.S17() // sync.WaitGroup
+		eg.S18() // atomic.AddInt32(&data, -1)
+		eg.S19() // reflect.TypeOf, reflect.ValueOf
+		eg.S20() // 동적함수
+		eg.S21() // Scanf
+		eg.S22() // Fscanf
+		eg.S23() // utf8.RuneLen
 	*/
-	eg.S24() //
+	eg.S24() //문자열
 }
