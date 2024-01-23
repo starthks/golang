1d40ed8ceb88ae57ec4832bfac685afea1be7d00
starthks <starthks@gmail.com>
2024-01-23 20:45:06 +0900

    cook10

---
+++ cook/cook10/pool/go.mod
@@ -0,0 +1,5 @@
+module cook/cook10/pool
+
+go 1.20
+
+require golang.org/x/crypto v0.18.0
--- earlygo/s25.go
+++ earlygo/s25.go
@@ -7,7 +7,12 @@ import (
 
 func S25() {
 	re1 := "[0-9]"
-	st1 := "Hello 100"
+	st1 := "Hello 100 foo"
 	ma, _ := regexp.MatchString(re1, st1 )
 	fmt.Println(ma)
+
+	re2 := regexp.MustCompile(`foo.?`)
+	fmt.Printf("%q\n", re2.Find([]byte(`sefood foo1`)))
+	fmt.Printf("%q\n", re2.Find([]byte(st1)))
+	
 }
